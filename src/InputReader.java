ee1a701da6b719d76333ce939fcecd9a00013353
jakubbrindza <jakub.brindza@gmail.com>
2016-02-11 18:33:15 +0000

    input

--- src/InputReader.java
+++ src/InputReader.java
@@ -4,6 +4,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Scanner;
+import java.io.File;
 
 /**
  * Created by Viliam Mihalik and Jakub Brindza on 6.2.2016.
@@ -12,6 +13,9 @@ public class InputReader {
     public List<String> lines;
     public int numberOfRows;
     public int numberOfColumns;
+    public int numberOfDrones;
+    public int numberOfTurns;
+    public int maxPayload;
     public Path file;
 
     public InputReader(String in) {
@@ -28,5 +32,32 @@ public class InputReader {
         Scanner s = new Scanner(lines.get(0));
         numberOfRows  = s.nextInt();
         numberOfColumns  = s.nextInt();
+        numberOfDrones = s.nextInt();
+        numberOfTurns = s.nextInt();
+        maxPayload = s.nextInt();
+
+        Scanner s1 = new Scanner(lines.get(1));
+        int[] productWeights = int[s1.nextInt()];
+
+
+        Scanner s2 = new Scanner(lines.get(2));
+        for (int i = 0; i < productWeights.length; i++){
+            productWeights[i] = s2.nextInt();
+        }
+
+
+
     }
+}
+
+public class Warehouse{
+
+    public int x;
+    public int y;
+
+    public Warehouse(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
 }
